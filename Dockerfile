e1a54f781789a9c7f5b990c948101d94b57a59c5
Harrison Wilkinson <coleinbrentwood@gmail.com>
2024-07-12 10:39:23 -0500

    using stuff

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-
 FROM tensorflow/tensorflow:2.13.0-gpu
-
 WORKDIR /tf
-
 RUN apt-get update && apt-get install -y \
     libgl1-mesa-glx \
-    libglib2.0-0
-
+    libglib2.0-0 \
+    libv4l-dev \
+    v4l-utils
 RUN pip install --no-cache-dir transformers torch pandas numpy jupyter wget ipykernel mediapipe opencv-python
-
+RUN usermod -aG video root
 CMD ["jupyter", "notebook", "--notebook-dir=/tf", "--ip=0.0.0.0", "--no-browser", "--allow-root"]
--- start.sh
+++ start.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
 # Set NUMA node for all PCI devices to 0
 echo "Setting NUMA node to 0 for all PCI devices..."
 for a in /sys/bus/pci/devices/*; do
     echo 0 | sudo tee $a/numa_node > /dev/null
 done
-
 echo "Starting TensorFlow Jupyter Notebook..."
 docker run -d \
-  \
   --name tf \
   --gpus all \
-  -v $(pwd):/tf\
+  -v $(pwd):/tf \
   -e TF_ENABLE_ONEDNN_OPTS=0 \
+  --device=/dev/video0:/dev/video0 \
+  --device=/dev/video1:/dev/video1 \
   tensorflow/tensorflow:2.13.0-gpu
-
